0d4bd1c8b60f9cddef27f4e4c42d65efb5e3e186
FilipeDiasLima <filipdiasdelima@gmail.com>
2020-08-13 02:20:40 -0300

    open note route added

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -3,14 +3,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import NotesList from './Pages/NotesList'
+import OpenNote from './Pages/OpenNote'
 
 const { Navigator, Screen } = createStackNavigator();
 
-const Routes = () => {
+function Routes() {
   return (
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name="NoteList" component={NotesList} />
+        <Screen name="OpenNote" component={OpenNote} />
       </Navigator>
     </NavigationContainer>
   );
